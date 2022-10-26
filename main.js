723a7df4346eecae0517fff5a3bb8ee6cd4af786
ihccenen <110120407+ihccenen@users.noreply.github.com>
2022-10-26 19:17:39 -0300

    Initial files

---
+++ main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+consoless.log('test')
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGVzcy5sb2coJ3Rlc3QnKVxyXG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
