74cb7287adf99f3efe024e713a185a5e6ea8fd90
ihccenen <110120407+ihccenen@users.noreply.github.com>
2022-10-30 14:40:53 -0300

    Added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,4 +11,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.ttf$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 }
